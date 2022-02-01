b5ed60439dc1c41c1d7bbbd20a43aee3bfb3dd02
Tatiana <tatismayo@hotmail.com>
2022-01-31 19:30:24 -0500

    operadoresAritmeticos

---
+++ operadoresAritmeticos.php
@@ -0,0 +1,22 @@
+<?php 
+$n1 = 2;
+$n2 = 5;
+$suma = $n1 + $n2;
+$resta = $n1 - $n2;
+$multiplicacion = $n1 * $n2;
+$division = $n1 / $n2;
+
+echo "La suma es:  ".$suma;
+echo "<br>";
+
+echo "La multiplicacion es:  ".$multiplicacion;
+echo "<br>";
+
+echo "La resta es:  ".$resta;
+echo "<br>";
+
+echo "La division es: ".$division;
+echo "<br>";
+
+
+
