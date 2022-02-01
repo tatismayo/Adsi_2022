7d4c1fbd44c7d32d16ee11e83d49f55fec579e73
Tatiana <tatismayo@hotmail.com>
2022-01-31 21:02:23 -0500

    positivoNegativo

---
+++ positivoNegativo.php
@@ -0,0 +1,10 @@
+<?php
+
+$numero = -5;
+if ($numero >0){
+    echo "El numero  ".$numero." es Positivo";
+}elseif($numero <0){
+        echo "El numero  ".$numero. " es Negativo";
+    }else {
+        echo "El numero es igual a Cero";
+    }
